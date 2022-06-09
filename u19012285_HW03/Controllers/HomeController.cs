e004ff6f7a01fbd5f3ab10ab787b42cedd6e78cd
Tasmiyah143 <62009864+Tasmiyah143@users.noreply.github.com>
2022-06-09 09:52:32 +0200

    These files are all for Homework 3

--- u19012285_HW03/Controllers/HomeController.cs
+++ u19012285_HW03/Controllers/HomeController.cs
@@ -1,30 +1,173 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using u19012285_HW03.Models;
+
 
 namespace u19012285_HW03.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file, FormCollection collections)
         {
-            ViewBag.Message = "Your application description page.";
+            string SelectedFile = Convert.ToString(collections["FileOption"]);
 
+            if(SelectedFile == "Document")
+            {
+                file.SaveAs(Path.Combine(HttpContext.Server.MapPath("~/Documents/ "), file.FileName));
+            }
+            else if (SelectedFile == "Image")
+            {
+                file.SaveAs(Path.Combine(HttpContext.Server.MapPath("~/Images/ "), file.FileName));
+            }
+            else
+            {
+                file.SaveAs(Path.Combine(HttpContext.Server.MapPath("~/Videos/ "), file.FileName));
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult File()
+        {
+            List<FileModel> UploadedFiles = new List<FileModel>();
+            string[] Documents = Directory.GetFiles(Server.MapPath("~/Documents/ "));
+           // string[] Images = Directory.GetFiles(Server.MapPath("~/Images/ "));
+            //string[] Videos = Directory.GetFiles(Server.MapPath("~/Videos/ "));
+
+            foreach(var document in Documents)
+            {
+               FileModel RetrieveFile = new FileModel();
+                RetrieveFile.FileName = Path.GetFileName(document);
+                RetrieveFile.FileType = "Document";
+                UploadedFiles.Add(RetrieveFile);
+            }
+
+           // foreach (var image in Images)
+           // {
+              //  FileModel RetrieveFile = new FileModel();
+               // RetrieveFile.FileName = Path.GetFileName(image);
+              //  myFile.Add(RetrieveFile);
+           /// }
+///
+            //foreach (var video in Videos)
+            //{
+             //   FileModel RetrieveFile = new FileModel();
+            //    RetrieveFile.FileName = Path.GetFileName(video);
+             //   myFile.Add(RetrieveFile);
+           // }
+
+            return View(UploadedFiles);
+        }
+
+        public ActionResult Image()
+        {
+            List<FileModel> UploadedImages = new List<FileModel>();
+            string[] Images = Directory.GetFiles(Server.MapPath("~/Images/ "));
+            foreach (var images in Images)
+            {
+                FileModel RetrieveImage = new FileModel();
+                RetrieveImage.FileName = Path.GetFileName(images);
+                RetrieveImage.FileType = "Image";
+                UploadedImages.Add(RetrieveImage);
+            }
+            return View(UploadedImages);
+        }
+
+        public ActionResult Video()
+        {
+            List<FileModel> UploadedVideos = new List<FileModel>();
+            string[] Videos = Directory.GetFiles(Server.MapPath("~/Videos/ "));
+            foreach (var videos in Videos)
+            {
+                FileModel RetrieveVideo = new FileModel();
+                RetrieveVideo.FileName = Path.GetFileName(videos);
+                RetrieveVideo.FileType = "Video";
+                UploadedVideos.Add(RetrieveVideo);
+            }
+            return View(UploadedVideos);
+
+        }
+
+        //public FileResult DownloadUploadedFile(string FileName, string FileType)
+        //{
+        // byte[] Housing = null; //ask
+        //// if(FileType == "Document")
+        //{
+        //    Housing = System.IO.File.ReadAllBytes(Server.MapPath("~/Documents/") + FileName);
+        //}
+        //else if(FileType == "Video")
+        // {
+        // //    Housing = System.IO.File.ReadAllBytes(Server.MapPath("~/Videos/") + FileName);
+        //}
+        // else
+        //{
+        //    Housing = System.IO.File.ReadAllBytes(Server.MapPath("~/Images/") + FileName);
+        //}
+
+        // return File(Housing, "application/octet-stream", FileName);
+        //}
+
+        public FileResult DownloadUploadedFile(string FileName)
+        {
+            
+
+            string download = Server.MapPath("~/Documents/") + FileName;
+
+            
+
+            byte[] bytes = System.IO.File.ReadAllBytes(download);
+
+
+            return File(bytes, "application/octet-stream", FileName);
+        }
+
+        public ActionResult DeleteUploadedFile(string FileName)
+        {
+            //string HousingDeletion = null;
+            // if (FileType == "Document")
+            //{
+            //  HousingDeletion = Server.MapPath("~/Documents/") + FileName;
+            //}
+            //else if (FileType == "Video")
+            //{
+            //  HousingDeletion = Server.MapPath("~/Videos/") + FileName;
+            //}
+            //else
+            //{
+            //  HousingDeletion = Server.MapPath("~/Images/") + FileName;
+            //}
+            //System.IO.File.Delete(HousingDeletion);
+            //return RedirectToAction("Index");
+            string delete = Server.MapPath("~/Documents/") + FileName;
+            byte[] bytes = System.IO.File.ReadAllBytes(delete);
+
+            System.IO.File.Delete(delete);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Aboutme()
+        {
+            
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //Image post action
+        
+       
 
-            return View();
-        }
     }
 }
---
+++ u19012285_HW03/Models/FileModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+
+namespace u19012285_HW03.Models
+{
+    public class FileModel
+    {
+        //public int ImageID { get; set; }
+        public string FileName { get; set; }
+        //[DisplayName("upload File")]
+        //public string ImagePath { get; set; }
+        public string FileType { get; set; }
+        //public IEnumerable<HttpPostedFileBase> myfiles { get; set; }
+      
+    }
+}
